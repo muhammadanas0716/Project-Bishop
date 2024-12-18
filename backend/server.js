8ab2c1c7f1b36b5c7fbb85ee653e368753179e76
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-12-18 18:09:33 +0400

    Refactor CORS middleware and add JSON parsing middleware

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,19 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
+// CORS Middleware
 app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
         ? process.env.FRONTEND_URL
-        : ["http://localhost:3000", "http://localhost:5173"],
-    credentials: true,
+        : ["http://localhost:3000", "http://localhost:5173"], // Allow dev URLs
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    credentials: true, // Allow cookies and credentials
   })
 );
+
+// Middleware for parsing JSON
 app.use(bodyParser.json());
 
 // MongoDB Connection URI
